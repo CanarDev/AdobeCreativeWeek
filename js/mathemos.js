accf7e6b5ed9257320095bd9ff81a99da81a589a
CanarDev <vincent.michel@edu.devinci.Fr>
2022-11-18 08:07:22 +0100

    :sparkles: first enigma in dev

--- js/mathemos.js
+++ js/mathemos.js
@@ -1,7 +1,34 @@
 const topInput = document.querySelector('.Top')
-const rightInput = document.querySelector('.Right')
+const leftInput = document.querySelector('.Left')
+const key = document.getElementById('key')
+const validate = document.querySelector('.validateEnigma')
 
 
-topInput.addEventListener('change', () => {
-    
+topInput.addEventListener('change', (e) => {
+    key.style.top = topInput.value + "px"
+
 })
+
+leftInput.addEventListener('change', (e) => {
+    key.style.left = leftInput.value + "px"
+})
+
+if (validate) {
+    var manyTimes = 0
+    validate.addEventListener("click", (e) => {
+        manyTimes += 1
+        console.log(topInput.value)
+        if (topInput.value == 400) {
+            if (leftInput.value == 550) {
+                alert("Tu as trouvé dévérouillé la serrure au bout de " + manyTimes + " fois!!")
+                validateThree.href = "/mathemos-enigma3.html"
+            }
+            else{
+                alert("Il ne te reste plus qu'a avancer la clef !")
+            }
+        }
+        else{
+            alert("Mauvaise réponse ! tu peux réessayer ;)")
+        }
+    })
+}
